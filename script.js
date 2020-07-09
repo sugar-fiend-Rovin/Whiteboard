e0cb42eff963967b223cde55fa66ef313884f22d
sugar-fiend-Rovin <roviny@uci.edu>
2020-07-08 23:58:45 -0700

    fixed rectangle tool

--- script.js
+++ script.js
@@ -13,95 +13,125 @@ var width = 0;
 var height = 0;
 var offset = $("#whiteboard").offset();
 
-$(document).ready(function(){
+$(document).ready(function () {
   $("#whiteboard").on({
-
     // when mouse is clicked
-    mousedown: function(event){
-    mousedown = true;
-    redo = [];
-    context.moveTo(event.pageX- offset.left,event.pageY - offset.top);
-    context.beginPath();
-    if( rectangle || circle)
-    {
-      starting_x = event.pageX- offset.left;
-      starting_y = event.pageY - offset.top;
-    }
-    // context.beginPath();
-    if (!eraser) {
-      context.strokeStyle = $("#colors")[0].value;
-    }
-    context.lineWidth = $("#brushsize")[0].value;
+    mousedown: function (event) {
+      mousedown = true;
+      redo = [];
+      context.moveTo(event.pageX - offset.left, event.pageY - offset.top);
+      context.beginPath();
+      if (rectangle || circle) {
+        starting_x = event.pageX - offset.left;
+        starting_y = event.pageY - offset.top;
+      }
+      // context.beginPath();
+      if (!eraser) {
+        context.strokeStyle = $("#colors")[0].value;
+      }
+      context.lineWidth = $("#brushsize")[0].value;
     },
 
     // when mouse is released
-    mouseup: function(){
-    mousedown = false;
-    width = 0;
-    height = 0;
-    radius = 0;
-    undo.push($("#whiteboard")[0].toDataURL());
-
+    mouseup: function () {
+      mousedown = false;
+      width = 0;
+      height = 0;
+      radius = 0;
+      undo.push($("#whiteboard")[0].toDataURL());
     },
 
     // when the mouse moves
-    mousemove: function(event){
+    mousemove: function (event) {
       if (mousedown == true) {
         if (rectangle) {
           if (width < 0 && height < 0) {
-            context.clearRect(starting_x + 1, starting_y + 1, width -2, height - 2);
+            context.clearRect(
+              starting_x + 1 * $("#brushsize")[0].value,
+              starting_y + 1 * $("#brushsize")[0].value,
+              width - 2 * $("#brushsize")[0].value,
+              height - 2 * $("#brushsize")[0].value
+            );
           } else if (width < 0) {
-            context.clearRect(starting_x + 1, starting_y - 1, width - 2, height + 2);
+            context.clearRect(
+              starting_x + 1 * $("#brushsize")[0].value,
+              starting_y - 1 * $("#brushsize")[0].value,
+              width - 2 * $("#brushsize")[0].value,
+              height + 2 * $("#brushsize")[0].value
+            );
           } else if (height < 0) {
-            context.clearRect(starting_x - 1, starting_y + 1, width + 2, height - 2);
-          } else{
-            context.clearRect(starting_x - 1, starting_y - 1, width + 2, height + 2);
+            context.clearRect(
+              starting_x - 1 * $("#brushsize")[0].value,
+              starting_y + 1 * $("#brushsize")[0].value,
+              width + 2 * $("#brushsize")[0].value,
+              height - 2 * $("#brushsize")[0].value
+            );
+          } else {
+            context.clearRect(
+              starting_x - 1 * $("#brushsize")[0].value,
+              starting_y - 1 * $("#brushsize")[0].value,
+              width + 2 * $("#brushsize")[0].value,
+              height + 2 * $("#brushsize")[0].value
+            );
           }
           width = event.pageX - offset.left - starting_x;
-          height = event.pageY - offset.top- starting_y;
+          height = event.pageY - offset.top - starting_y;
           context.strokeRect(starting_x, starting_y, width, height);
-
-
-        } 
-        else if (circle) {
+        } else if (circle) {
           context.beginPath();
-          context.clearRect(starting_x - radius - 1, starting_y - radius - 1, radius * 2 + 2, radius * 2 + 2);
-          radius = Math.hypot(event.pageX-offset.left - starting_x,event.pageY - offset.top - starting_y);
-          context.arc(starting_x, starting_y, Math.hypot(event.pageX-offset.left - starting_x,event.pageY - offset.top - starting_y), 0, 2*Math.PI,false);
+          context.clearRect(
+            starting_x - radius - 1,
+            starting_y - radius - 1,
+            radius * 2 + 2,
+            radius * 2 + 2
+          );
+          radius = Math.hypot(
+            event.pageX - offset.left - starting_x,
+            event.pageY - offset.top - starting_y
+          );
+          context.arc(
+            starting_x,
+            starting_y,
+            Math.hypot(
+              event.pageX - offset.left - starting_x,
+              event.pageY - offset.top - starting_y
+            ),
+            0,
+            2 * Math.PI,
+            false
+          );
           context.stroke();
           context.closePath();
-        }
-        else {
-          context.lineTo(event.pageX-offset.left,event.pageY - offset.top);
+        } else {
+          context.lineTo(event.pageX - offset.left, event.pageY - offset.top);
           context.stroke();
         }
       }
-    }
+    },
   });
 
   //clear button
-  $("#clear").click(function(){
+  $("#clear").click(function () {
     context.clearRect(0, 0, 1900, 800);
   });
 
   //eraser
-  $("#eraser").click(function(){
+  $("#eraser").click(function () {
     context.strokeStyle = "white";
     eraser = true;
     rectangle = false;
     circle = false;
-
   });
 
   // revert back to brush
-  $("#brush").click(function(){
+  $("#brush").click(function () {
     eraser = false;
     rectangle = false;
     circle = false;
   });
 
   // draw rectangles
-  $("#rectangle").click(function(){
+  $("#rectangle").click(function () {
     if (eraser || circle) {
       eraser = false;
       circle = false;
@@ -110,7 +140,7 @@ $(document).ready(function(){
   });
 
   // draw circles
-  $("#circle").click(function(){
+  $("#circle").click(function () {
     if (eraser || rectangle) {
       eraser = false;
       rectangle = false;
@@ -119,36 +149,36 @@ $(document).ready(function(){
   });
 
   // undo
-  $("#undo").click(function(){
+  $("#undo").click(function () {
     if (undo.length > 1) {
       var dataURL = undo.pop();
       redo.push(dataURL);
       dataURL = undo.pop();
-      undo.push(dataURL)
+      undo.push(dataURL);
       var image = new Image();
-      image.onload = function(){
+      image.onload = function () {
         context.clearRect(0, 0, 1900, 800);
-        context.drawImage(image,0, 0, 1900, 800);
+        context.drawImage(image, 0, 0, 1900, 800);
       };
       image.src = dataURL;
     }
   });
 
   //redo
-  $("#redo").click(function(){
-    if (redo.length){
-    var dataURL = redo.pop();
-    undo.push(dataURL);
-    var image = new Image();
-    image.onload = function(){
-      context.clearRect(0, 0, 1900, 800);
-      context.drawImage(image,0, 0, 1900, 800);
-    };
-    image.src = dataURL;
+  $("#redo").click(function () {
+    if (redo.length) {
+      var dataURL = redo.pop();
+      undo.push(dataURL);
+      var image = new Image();
+      image.onload = function () {
+        context.clearRect(0, 0, 1900, 800);
+        context.drawImage(image, 0, 0, 1900, 800);
+      };
+      image.src = dataURL;
     }
   });
 
-  $(".dropbtn").click(function(){
+  $(".dropbtn").click(function () {
     $("myDropdown").classList.toggle("show");
   });
 
